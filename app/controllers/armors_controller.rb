e9d680862c8a780b6c8df9fe2cb9151ab8736504
Justin Horton <justinhorton125@gmail.com>
2021-04-22 01:46:35 -0500

    Update armors_controller.rb
    Updated the armors controller to include variables used in the search, navbar, and in the armors partial that is used in the armors pages

--- app/controllers/armors_controller.rb
+++ app/controllers/armors_controller.rb
@@ -1,9 +1,48 @@
 class ArmorsController < InheritedResources::Base
 
+  include ActionView::Helpers::NumberHelper
+
+  add_breadcrumb "Home", :root_path
+
+  def index
+    @armor_categories = ArmorCategory.all
+    @filter = params[:filter]
+    if @filter != nil
+      @armors = Armor.where(['armorCategory_id = ?', "#{@filter.to_i}"])
+    else
+      @armors = Armor.all
+    end
+    add_breadcrumb "Armors", armors_path
+  end
+
+  def show
+    @armor_categories = ArmorCategory.all
+    @armor = Armor.find(params[:id])
+
+    add_breadcrumb "Armors", armors_path
+    add_breadcrumb "#{@armor.name}", "#{@armor.id}"
+  end
+
+  def search_results
+    @query = params[:query]
+    @category = params[:category]
+    @armor_categories = ArmorCategory.all
+
+    if @category != ""
+      @armors = Armor.where(['Name LIKE ? AND armorCategory_id = ?', "%#{@query}%", "#{@category.to_i}"])
+    else
+      @armors = Armor.where(['Name LIKE ?', "%#{@query}%"])
+    end
+
+
+  end
+
   private
 
     def armor_params
       params.require(:armor).permit(:armor, :concealment, :speed, :dodge, :steadiness, :stamina, :description, :armorCategory)
     end
+
+
 
 end
