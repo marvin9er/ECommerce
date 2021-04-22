134c026131c732d73a790f2449ac8523b6712a2e
Justin Horton <justinhorton125@gmail.com>
2021-04-22 01:54:34 -0500

    Update routes.rb
    Updated routes file to add new tables and custom routes to use on website pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
+  resources :contacts
+  resources :abouts
+  root to: 'pages#home'
+
+  get "/about", to: "pages#about"
+  get "/contact", to: "pages#contact"
+
+
+  resources :users
+  resources :armor_orders
+  resources :orders
   resources :armor_categories
-  resources :armors
+  resources :armors do
+    collection do
+      get 'search_results'
+    end
+  end
+
   resources :categories
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
